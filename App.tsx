cce6c262cd4f21cb20aa09bc324fe426dbf0043d
FabianPineda <fban.pnd@gmail.com>
2023-12-18 16:25:01 -0600

    Vista FORM

--- App.tsx
+++ App.tsx
@@ -5,114 +5,64 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Formulario from './componentes/Formulario';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const [ busqueda, guardarBusqueda ] = useState( {ciudad : '', pais : ''} )
+  const [ consultar, guardarConsultar ] = useState(false)
+  const { ciudad, pais } = busqueda
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect( () => {
+    const consultarCLima = async () => {
+      if (consultar) {
+        const appId = '152e2adf54a69a96dcd32a4e6bdde0fc'
+        const url =  `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais},{country code}&appid=${appId}` 
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+        try {
+          const respuesta = await fetch(url)
+          const resultado = await respuesta.json()
+          console.log(resultado)
+        } catch (error) {
+          mostrarAlerta()
+          
+        }   
+      }
+    }
+    
+    consultarCLima()
+
+  }, [consultar] )
+
+  const mostrarAlerta = () => {
+    Alert.alert( 'Error', 'No hay resultados, intenta con otra ciudad o país' )   
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+        <View style={styles.app} >
+          <View style={styles.contenido} >
+            <Formulario busqueda={busqueda} guardarBusqueda={guardarBusqueda} guardarConsultar={guardarConsultar} />
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </TouchableWithoutFeedback>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  app : {
+    flex : 1,
+    backgroundColor : 'rgb(71, 149, 212)',
+    justifyContent : 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  contenido : {
+    marginHorizontal : '2.5%'
+  }
 });
 
 export default App;
